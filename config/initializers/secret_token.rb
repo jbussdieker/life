4abaa6f8a8485d4b515c89f58d57bd8f1f6085f4
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-05-27 18:12:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Life::Application.config.secret_token = '3b1e011d56ab31832d68fd6cf99c07fac9b95b29543fb38ce8213a67d398df905dc3a6da36901b90e542a2cfe55388f1f3a6ab96433112a4708043737bb68a4a'
